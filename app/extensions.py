7c11f92720ffa0f9d0e68ac919bf8db4baab79b5
brettsky911 <brettsky911@gmail.com>
2025-10-01 22:36:44 -0400

    update gitignore and add google oauth

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from authlib.integrations.flask_client import OAuth
+#import flask_talisman
+
 #import the database and the CSRF protection
 db = SQLAlchemy()
 csrf = CSRFProtect()
+oauth = OAuth()
 
 
 
